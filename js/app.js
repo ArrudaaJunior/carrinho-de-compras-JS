78a8fd87735549bf2d09fccaf2a3dc92b6e28ae8
Arruda Junior <juniorarruda15@gmail.com>
2024-05-17 20:55:05 -0300

    Calculando o valor do produto

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 function adicionar() {
-    let nomeProduto = document.getElementById('produto');
+    let produto = document.getElementById('produto').value;
+    let nomeDoProduto = produto.split('-')[0];
     let quantidade = document.getElementById('quantidade');
-
+    let valorDaUnidade = produto.split('R$')[1];
+    let preco = quantidade.value * valorDaUnidade;
 }
-
 
 function limpar() {
 
