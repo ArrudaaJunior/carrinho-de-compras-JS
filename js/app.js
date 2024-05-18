652704021a63523df0400c709435e39e72da65dd
Arruda Junior <juniorarruda15@gmail.com>
2024-05-18 10:20:11 -0300

    Adicionando produto no carrinho

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 function adicionar() {
+    
+    //Recuperando valores do html
     let produto = document.getElementById('produto').value;
     let nomeDoProduto = produto.split('-')[0];
-    let quantidade = document.getElementById('quantidade');
+    let quantidade = document.getElementById('quantidade').value;
     let valorDaUnidade = produto.split('R$')[1];
-    let preco = quantidade.value * valorDaUnidade;
+    
+    //Calculando preço e subtotal
+    let preco = quantidade * valorDaUnidade;
+
+    //Adicionando no carrinho
+    let listaProdutos = document.getElementById('lista-produtos');
+    listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
+    <section class="carrinho__produtos__produto">
+      <span class="texto-azul">${quantidade}x</span> ${nomeDoProduto} <span class="texto-azul">R$${preco}</span>
+    </section>`;
+
+    //Atualizando o valor total
 }
 
 function limpar() {
